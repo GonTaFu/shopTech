44f30b15c30ad090e0e015fe2d4d74a3d77fcbd9
GonTaFu <trongnhan10a11@gmail.com>
2025-03-31 17:05:15 +0700

    chỉnh sửa lại paybill

--- my-fontend/src/app/checkout/page.js
+++ my-fontend/src/app/checkout/page.js
@@ -1,23 +1,12 @@
 'use client';
 import Image from "next/image";
-import PayBill from "../../components/paybill.js";
+import PayBill from "../../components/paybill";
 import { Container } from "@mui/material";
 
 export default function payPage() {
     return (
-        <Container maxWidth="md" sx={{ height: 100 }}>
-
-            <h1>Thanh toán</h1>
+        <div>
             <PayBill></PayBill>
-
-
-            <style jsx>{`
-            h1 {
-                align-items: center;
-                justify-content: center;
-                display: flex;
-                },
-                `}</style>
-        </Container>
+        </div>
     );
 }
