938b192327d044413cbd3ada371ae0914cea1ea6
GonTaFu <trongnhan10a11@gmail.com>
2025-04-01 10:03:58 +0700

    chỉnh tên file

--- my-fontend/src/app/productlist/page.js
+++ my-fontend/src/app/productlist/page.js
@@ -1,6 +1,6 @@
 'use client';
 import Image from "next/image";
-import PageProductList  from "../../components/productlist";
+import PageProductList  from "../../components/ProductList";
 
 export default function ProductList() {
     return (
