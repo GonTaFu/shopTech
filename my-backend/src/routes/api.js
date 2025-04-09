2bc9f595c3e5f8f447321f027b9ef88dccfe0d20
Comotcaiten <pikachu123450vn@gmail.com>
2025-04-10 00:58:12 +0700

    Phúc: Xong BrandController với sự giúp sức của ChatGPT, It is very useful than ... .

--- my-backend/src/routes/api.js
+++ my-backend/src/routes/api.js
@@ -28,6 +28,22 @@ var ordersDetailController = require('../controllers/OrdersDetailController')
 // // Get - Read
 // router.get("/categories", categoriesController.GetCategories);
 
+// Brands
+// Add - Create
+router.post("/brands/add", brandsController.create);
+
+// Update
+router.put("/brands/update/:id", brandsController.update);
+
+// Delete
+router.delete("/brands/delete/:id", brandsController.delete);
+
+// Get by ID
+router.get("/brands/:id", brandsController.getById);
+
+// Get al
+router.get("/brands", brandsController.getAll);
+
 
 // 
 router.get("/", (req, res) => {
