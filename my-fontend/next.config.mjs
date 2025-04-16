0cd21f3de6b03dfa600cc8dad2b2800e90029778
GonTaFu <trongnhan10a11@gmail.com>
2025-04-16 23:15:00 +0700

    đẩy về cấu hình gốc next.config

--- my-fontend/next.config.mjs
+++ my-fontend/next.config.mjs
@@ -1,21 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: {}, // Nếu cần dùng Server Actions
-  },
-  server: {
-    port: process.env.PORT || 3000, // Đảm bảo sử dụng đúng cổng trên Render
-  },
-};
-  
-  export default nextConfig;
-
-//cầu hình gốc
+// Thêm Server Port mà không nhận được
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   experimental: {
 //     serverActions: {}, // Nếu cần dùng Server Actions
 //   },
+//   server: {
+//     port: process.env.PORT || 3000, // Đảm bảo sử dụng đúng cổng trên Render
+//   },
 // };
+  
+//   export default nextConfig;
 
-// export default nextConfig;
+//Cấu hình gốc
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {}, // Nếu cần dùng Server Actions
+  },
+};
+
+export default nextConfig;
