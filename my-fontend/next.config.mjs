912a92a5dca69d8d342bf1069be7206f98308598
GonTaFu <trongnhan10a11@gmail.com>
2025-04-16 23:00:09 +0700

    chỉnh next.config.mjs

--- my-fontend/next.config.mjs
+++ my-fontend/next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: {}, // Nếu cần dùng Server Actions
-    },
-  };
+  experimental: {
+    serverActions: {}, // Nếu cần dùng Server Actions
+  },
+  server: {
+    port: process.env.PORT || 3000, // Đảm bảo sử dụng đúng cổng trên Render
+  },
+};
   
   export default nextConfig;
-  
+
+//cầu hình gốc
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverActions: {}, // Nếu cần dùng Server Actions
+//   },
+// };
+
+// export default nextConfig;
