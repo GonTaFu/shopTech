776e529b2077a1757a9a972991f560afa802a045
GonTaFu <trongnhan10a11@gmail.com>
2025-04-18 09:03:49 +0700

    lỗi searching khi deploy

--- my-fontend/src/app/products/search/page.js
+++ my-fontend/src/app/products/search/page.js
@@ -1,12 +1,15 @@
 'use client';
 import Image from "next/image";
-import ProductListSearch  from "../../../components/ProductListSearch";
+import ProductListSearch from "../../../components/ProductListSearch";
 import { Container } from "@mui/material";
+import { Suspense } from "react";
 
 export default function ProductList() {
-    return (
-        <Container maxWidth="lg" sx={{ marginBottom: 30 }}>
-            <ProductListSearch></ProductListSearch>
-        </Container>
-    );
+  return (
+    <Container maxWidth="lg" sx={{ marginBottom: 30 }}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductListSearch />
+      </Suspense>
+    </Container>
+  );
 }
